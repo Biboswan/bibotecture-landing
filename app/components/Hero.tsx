5f5196b08948232f65579a5ae40d7386ab93426e
Biboswan Roy <biboswan98@gmail.com>
2025-05-23 00:47:08 +0530

    fix layout in scroll

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,12 +6,17 @@ export const Hero: React.FC = () => {
   return (
     <section className="mt-9 w-full max-w-[1320px] max-md:max-w-full">
       <div className="flex gap-5 max-md:flex-col">
+      <aside className="w-[27%] max-md:hidden">
+          <div className="flex gap-10 justify-between items-center text-lg leading-tight text-white mt-[573px] max-md:mt-10">
+            <FaArrowDown className="object-contain shrink-0 self-stretch my-auto w-12 aspect-square" style={{ fontSize: "24px" }} />
+            <p className="self-stretch my-auto">Scroll down</p>
+          </div>
+        </aside>
         <div className="ml-5 w-[73%] max-md:ml-0 max-md:w-full">
           <div className="grow max-md:mt-10 max-md:max-w-full">
             <div className="flex gap-5 max-md:flex-col">
               <div className="w-[70%] max-md:ml-0 max-md:w-full">
                 <div className="flex relative flex-col pt-28 pb-80 text-8xl font-bold text-white leading-[96px] min-h-[738px] max-md:py-24 max-md:max-w-full max-md:text-4xl max-md:leading-10">
-                 
                     <video
                         autoPlay
                         muted
@@ -38,12 +43,12 @@ export const Hero: React.FC = () => {
             </div>
           </div>
         </div>
-        <aside className="w-[27%] max-md:ml-0 max-md:w-full">
+        <div className="min-md:hidden w-full">
           <div className="flex gap-10 justify-between items-center text-lg leading-tight text-white mt-[573px] max-md:mt-10">
             <FaArrowDown className="object-contain shrink-0 self-stretch my-auto w-12 aspect-square" style={{ fontSize: "24px" }} />
             <p className="self-stretch my-auto">Scroll down</p>
           </div>
-        </aside>
+        </div>
       </div>
     </section>
   );
